e4c7474f4f021c8a906eb697879124c5413168bb
Radmir <gerasimoffdg@gmail.com>
2021-05-22 20:40:29 +0300

    домашка 7

---
+++ HomeWork7/Launcher.java
@@ -0,0 +1,10 @@
+package HW7;
+
+public class Launcher {
+
+    public static void main(String[] args) { // главный метод мэин с которого будем запускать
+
+        MainWindow mainWindow = new MainWindow();
+
+    }
+}
