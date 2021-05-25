b1ee3afc857656b5faf52c2163f4168ead7d0076
Radmir <gerasimoffdg@gmail.com>
2021-05-25 12:52:47 +0300

    Прошла лекция 8

---
+++ Les8/Launcher.java
@@ -0,0 +1,16 @@
+package Lection08.app;
+
+/**
+ * Created by Aleksandr Gladkov [Anticisco]
+ * Date: 17.05.2021
+ */
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        MainWindow mainWindow = new MainWindow();
+
+    }
+
+}
